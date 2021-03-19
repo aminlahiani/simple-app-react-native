567d70ad0ea41ec53c63c6c09cfd4916aa55c4f1
amin lahiai <aminlahiani@outlook.com>
2021-03-19 13:22:52 +0100

    TextInput

--- src/screens/InputSreen.js
+++ src/screens/InputSreen.js
@@ -4,13 +4,15 @@ import { SafeAreaView, StyleSheet, TextInput, Text } from "react-native";
 const UselessTextInput = () => {
   const [text, onChangeText] = React.useState("");
   const [number, onChangeNumber] = React.useState(null);
-
+  const [password, setPassword] = React.useState('');
   return (
     <SafeAreaView>
       <TextInput
         style={styles.input}
         onChangeText={onChangeText}
         value={text}
+        autoCapitalize="none"
+        autoCorrect={false}
       />
       <TextInput
         style={styles.input}
@@ -21,6 +23,15 @@ const UselessTextInput = () => {
       />
       <Text>My name is {text} </Text>
       <Text>My age is {number} </Text>
+      <Text>Enter Password:</Text>
+      <TextInput
+        style={styles.inputpass}
+        autoCapitalize="none"
+        autoCorrect={false}
+        value={password}
+        onChangeText={newValue => setPassword(newValue)}
+      />
+      {password.length < 4 ? <Text>Password must be 4 characters</Text> : null}
     </SafeAreaView>
   );
 };
@@ -31,6 +42,11 @@ const styles = StyleSheet.create({
     margin: 12,
     borderWidth: 1,
   },
+  inputpass: {
+    margin: 15,
+    borderColor: 'black',
+    borderWidth: 1
+  }
 });
 
 export default UselessTextInput;
